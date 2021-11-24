0016bac821b764a61ac2735498b4f481aa71bde5
Shubh9907 <Shubhamv60047@gmail.com>
2021-11-24 19:34:22 +0530

    Added:- Code added to calculate fare of a ride

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -4,7 +4,13 @@ public class CabInvoiceGenerator {
     private static final int COST_PER_KM = 10;
     private static final int COST_PER_MINUTES = 1 ;
 
+    /*
+    Method to calculate fare
+    @Param distance, time
+     */
     public double calculateFare(double distance, int time) {
-        return distance*COST_PER_KM + time*COST_PER_MINUTES;
+        double fare =  distance*COST_PER_KM + time*COST_PER_MINUTES;
+        if (fare>5) return fare;
+        else return 5;
     }
 }
--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -1,14 +1,23 @@
 package com.bridgelabz;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CabInvoiceGeneratorTest {
 
+    static CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+
     @Test
     public void GivenDistanceAndTime_ShouldReturnTotalFare() {
-        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
         double fare = cabInvoiceGenerator.calculateFare(2,5);
         Assertions.assertEquals(25,fare);
+    }
+
+    @Test
+    void GivenDistanceAndTime_WhenLess_ShouldReturnMinimumFare() {
+        double fare = cabInvoiceGenerator.calculateFare(0.2,1);
+        Assertions.assertEquals(5,fare);
     }
 }
